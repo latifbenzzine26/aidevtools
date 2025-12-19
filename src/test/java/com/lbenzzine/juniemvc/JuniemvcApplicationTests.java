473ebbf75f82bcdf8c43f305fd9abefecf92ef55
L <benzzine.channel1@gmail.com>
2025-12-19 16:30:42 -0600

    Initial commit

---
+++ src/test/java/com/lbenzzine/juniemvc/JuniemvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbenzzine.juniemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniemvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
